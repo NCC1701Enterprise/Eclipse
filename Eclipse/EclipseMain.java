e5f76a2e45c86c29b2788bb329dbbf4794f84106
Yancheng Du 2015 <yahooyancheng@gmail.com>
2015-05-11 14:42:51 -0500

    initial push

---
+++ Eclipse/EclipseMain.java
@@ -0,0 +1,30 @@
+/**
+ * Eclipse Main Game, this is where the main is run
+ * By Kevin Du
+ */
+public class EclipseMain
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class EclipseMain
+     */
+    public EclipseMain()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}

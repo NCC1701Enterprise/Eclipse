0cfb19d2f731baa07e9559e65ae189bc38b34ae3
NCC1701Enterirpse <yandu5@students.d125.org>
2015-05-18 21:52:30 -0500

    I think this will be most of the main classes I will need

---
+++ Eclipse/PlayerBoards.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of interface PlayerBoards here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface PlayerBoards
+{
+    /**
+     * An example of a method header - replace this comment with your own
+     * 
+     * @param  y    a sample parameter for a method
+     * @return        the result produced by sampleMethod 
+     */
+    int sampleMethod(int y);
+}
